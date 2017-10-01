956aeeedf5fcf37481e31b823f918e5eb6291da4
thassiov <tvmcarvalho@gmail.com>
2017-10-01 10:52:35 -0300

    TMUX: adding CPU and MEM information

---
+++ tmux-scripts/cpu_temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+case "$OSTYPE" in
+    linux-gnu)
+            if which sensors > /dev/null; then
+                sensors | grep Core | awk '{print $3;}' | grep -oEi '[0-9]+[0-9]+' | awk '{total+=$1; count+=1} END {print total/count "."}' | cut -d "." -f1
+            else
+                ""
+            fi
+        ;;
+esac
