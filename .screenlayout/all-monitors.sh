0d4bc2876e8d67277d1118b68142d940f3d13317
thassiov <tvmcarvalho@gmail.com>
2023-08-22 15:05:15 -0300

    screen layouts

---
+++ .screenlayout/all-monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 232x1440 --rate 59.88 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-3-1 --off --output DP-3-2 --primary --mode 2560x1440 --pos 0x0 --rate 59.95 --rotate normal --output DP-3-3 --mode 1920x1080 --pos 2560x0 --rate 59.94 --rotate left
