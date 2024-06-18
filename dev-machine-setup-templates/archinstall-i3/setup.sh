4d9bf82c6224845f41eda8d4bd978c0193345c57
thassiov <tvmcarvalho@gmail.com>
2024-06-18 18:06:25 -0300

    Updates setup

--- dev-machine-setup-templates/archinstall-i3/setup.sh
+++ dev-machine-setup-templates/archinstall-i3/setup.sh
@@ -14,7 +14,7 @@ echo "Setting up clean arch linux installation (done with archinstall)"
 
 echo
 echo "Installing git and base-devel for setting up yay"
-sudo pacman -S git base-devel --noconfirm
+sudo pacman -S base-devel --noconfirm
 
 echo
 echo "Updating certs to make git play nice"
@@ -43,12 +43,12 @@ sudo groupadd docker
 sudo gpasswd -a thassiov docker
 
 echo
-echo "Add user thassiov to video group"
-sudo gpasswd -a thassiov video
-
-echo
 echo "Add user thassiov to vboxusers group"
 sudo gpasswd -a thassiov vboxusers
+
+echo
+echo "Add user thassiov to video group"
+sudo gpasswd -a thassiov video
 
 echo
 echo "Enable vnstat"
@@ -71,41 +71,17 @@ sudo systemctl mask systemd-rfkill.socket
 sudo systemctl enable rfkill-unblock@all
 
 echo
-echo "Configure thassiov user oh-my-zsh and set zsh as default shell"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo
 echo "Clone dotfiles"
-git clone https://github.com/thassiov/dotfiles .dotfiles
+git clone https://github.com/thassiov/dotfiles $HOME/.dotfiles
 
 echo
 echo "Ensure .config directory exists"
 mkdir .config
 
 echo
-echo "Configure alacritty"
-ln -s ~/.dotfiles/alacritty ~/.config
-
-echo
-echo "Configure tmux"
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-
-echo
-echo "Configure zsh"
-mv ~/.zshrc ~/.zshrc.old
-ln -s ~/.dotfiles/.zshrc ~/
-ln -s ~/.dotfiles/.envs ~/
-ln -s ~/.dotfiles/.clialiases ~/
-ln -s ~/.dotfiles/.cli-functions ~/
-ln -s ~/.dotfiles/.zsh-keybindings ~/
-
-echo
-echo "Configure gitconfig"
-ln -s ~/.dotfiles/.gitconfig ~/
-
-echo
-echo "Configure neovim"
-ln -s ~/.dotfiles/nvim-configs/kickstart ~/.config/nvim
+echo "Setup configuration files for my applications (alacritty, i3, nvim, etc)"
+cd $HOME
+make config
 
 echo
 echo "Echoing machine interfaces"
@@ -116,8 +92,8 @@ echo "Manual tasks"
 echo "1 - Don't forget to set `USB_AUTOSUSPEND=0` at `/etc/tlp.conf` or else usb charging won't work [https://linrunner.de/en/tlp/docs/tlp-configuration.html#usb]"
 echo "2 - Check to see if the virtualbox daemon is enabled. You can enable it manually or simply restart the machine"
 echo "3 - Make sure to link `70-synaptics.conf` file at `/etc/X11/xorg.conf.d` to make the touchpad work"
-echo "4 - Make sure to link `.Xmodmap` at the $HOME directory if needed"
-echo "5 - Run the `kb/symbols/replace-files.sh` script to make the 60% keyboard work properly"
+echo "4 - Run the `kb/symbols/replace-files.sh` script to make the 60% keyboard work properly"
+echo "5 - And don't forget your ssh keys!"
 
 echo
 echo "Done. Happy hacking!"
