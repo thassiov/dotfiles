dc7a2991db2437ab9f8e6360e76c8899176ace6e
thassiov <tvmcarvalho@gmail.com>
2024-07-25 12:10:47 -0300

    Adds close-buffers plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -268,6 +268,7 @@ require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
+	"kazhala/close-buffers.nvim",
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.
