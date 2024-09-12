1fb2cd927e68575b1b57f254187133edc19f8e82
thassiov <tvmcarvalho@gmail.com>
2024-09-12 18:56:33 -0300

    Adds screenlayout

---
+++ xrandr-screenlayout/.screenlayout/all-monitors-2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 431x1680 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-3-1 --off --output DP-3-2 --primary --mode 2560x1440 --pos 0x240 --rotate normal --output DP-3-3 --mode 1920x1080 --pos 2560x0 --rotate left
