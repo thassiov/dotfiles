bbaf583730045997817d499edce9a22f680b20d6
thassiov <tvmcarvalho@gmail.com>
2025-01-31 22:31:00 -0300

    Adds new screen layouts

---
+++ xrandr-screenlayout/.screenlayout/bigmonitor-dualup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --mode 2560x2880 --pos 2560x0 --rate 59.98 --rotate normal --output DP-4 --off --output DP-1-1 --off --output DP-1-2 --primary --mode 2560x1440 --pos 0x785 --rate 59.95 --rotate normal --output DP-1-3 --off
---
+++ xrandr-screenlayout/.screenlayout/dualup-only.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-1 --mode 2560x2880 --pos 0x0 --rate 59.98 --rotate normal --output DP-1-2 --off --output DP-1-3 --off
