cb80178237f63cc858b20adafb86296360468169
thassiov <tvmcarvalho@gmail.com>
2025-02-08 22:42:41 -0300

    Updates on screenlayout

--- xrandr-screenlayout/.screenlayout/notebook-bigmonitor-max-stacked.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --mode 2560x1600 --pos 0x1440 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-1 --off --output DP-1-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3 --off
