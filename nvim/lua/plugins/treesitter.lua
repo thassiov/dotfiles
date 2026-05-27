f0a2133299b9942c795eecd99a3d5cad986dfc92
thassiov <tvmcarvalho@gmail.com>
2026-05-26 21:50:20 -0300

    nvim: migrate nvim-treesitter to main branch
    The archived master branch is incompatible with Nvim 0.12: the custom
    set-lang-from-info-string! directive crashes in markdown buffers because
    Query:iter_matches now returns table-per-capture instead of single nodes.
    
    Switch to the rewritten main branch, install parsers via the new
    .install() API, and enable highlight + treesitter folding from a
    FileType autocmd.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,61 +1,62 @@
 -- Treesitter
--- Advanced syntax highlighting and code parsing
+-- Parser installer + queries for Neovim's built-in treesitter (nvim-treesitter main branch).
 
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "c",
-        "html",
-        "lua",
-        "markdown",
-        "vim",
-        "vimdoc",
-        "cpp",
-        "css",
-        "cmake",
-        "diff",
-        "csv",
-        "dockerfile",
-        "git_config",
-        "git_rebase",
-        "gitcommit",
-        "gitignore",
-        "graphql",
-        "hcl",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "javascript",
-        "luadoc",
-        "passwd",
-        "pem",
-        "python",
-        "rasi",
-        "readline",
-        "regex",
-        "rust",
-        "go",
-        "sql",
-        "ssh_config",
-        "terraform",
-        "toml",
-        "tmux",
-        "tsx",
-        "xml",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { "ruby" },
-      },
-      indent = { enable = false },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter").install({
+				"bash",
+				"c",
+				"html",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"vim",
+				"vimdoc",
+				"cpp",
+				"css",
+				"cmake",
+				"diff",
+				"csv",
+				"dockerfile",
+				"git_config",
+				"git_rebase",
+				"gitcommit",
+				"gitignore",
+				"graphql",
+				"hcl",
+				"jsdoc",
+				"json",
+				"javascript",
+				"luadoc",
+				"passwd",
+				"pem",
+				"python",
+				"rasi",
+				"readline",
+				"regex",
+				"rust",
+				"go",
+				"sql",
+				"ssh_config",
+				"terraform",
+				"toml",
+				"tmux",
+				"tsx",
+				"xml",
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					pcall(vim.treesitter.start, args.buf)
+					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo.foldmethod = "expr"
+				end,
+			})
+		end,
+	},
 }
